adacc35e07753807bcb7dd632d65b2c3bbbf698a
Morten Kristensen <msk@nullpointer.dk>
2011-03-12 15:09:49 +0100

    Added keyword spaces (aesthetic).

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -6,33 +6,33 @@ bool CPU::cpuidSupported() {
   // software can change the value of this flag, the CPUID instruction
   // is executable.
   unsigned int res;
-  __asm__("mov %%ecx, %%eax;"
-          "xor $200000, %%eax;"
-          "xor %%ecx, %%eax;"
-          "je no;"
-          "mov $1, %%eax;"
-          "jmp end;"
-          "no: mov $0, %%eax;"
-          "end:;"
-          : "=a" (res)
-          :
-          : "cc"); 
+  __asm__ ("mov %%ecx, %%eax;"
+           "xor $200000, %%eax;"
+           "xor %%ecx, %%eax;"
+           "je no;"
+           "mov $1, %%eax;"
+           "jmp end;"
+           "no: mov $0, %%eax;"
+           "end:;"
+           : "=a" (res)
+           :
+           : "cc"); 
   
   return res > 0;
 }
 
 void CPU::cpuid(unsigned int opcode, unsigned int result[4]) {
-  if(!cpuidSupported()) return;
+  if (!cpuidSupported()) return;
 
   // PIC compliant version
-  __asm__("cpuid;"
-          "movl %%ebx, %1;" 
-          : "=a" (result[0]), // EAX register -> result[0]
-            "=r" (result[1]), // EBX register -> result[1]
-            "=c" (result[2]), // ECX register -> result[2]
-            "=d" (result[3])  // EDX register -> result[3]
-          : "0" (opcode)
-          : "cc");
+  __asm__ ("cpuid;"
+           "movl %%ebx, %1;" 
+           : "=a" (result[0]), // EAX register -> result[0]
+             "=r" (result[1]), // EBX register -> result[1]
+             "=c" (result[2]), // ECX register -> result[2]
+             "=d" (result[3])  // EDX register -> result[3]
+           : "0" (opcode)
+           : "cc");
 }
 
 bool CPU::aesSupported() {
